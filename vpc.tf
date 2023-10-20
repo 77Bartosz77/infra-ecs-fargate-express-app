3941629fc750a2f5167262ab9de4912adb565b1c
77Bartosz77 <77BartoszCh77@gmail.com>
2023-10-20 14:31:11 +0200

    add internet gateway and route table to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -57,3 +57,36 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.express_app_vpc
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.express_app_vpc
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "route1" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.sn1.id
+}
+
+resource "aws_route_table_association" "route2" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.sn2.id
+}
+
+resource "aws_route_table_association" "route3" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.sn3.id
+}
